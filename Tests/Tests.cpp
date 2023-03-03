8fe8db48d65bd6188b65a73c32dda884798d9168
Damien Ronssin <damien.ronssin@gmail.com>
2023-03-03 14:56:56 +0100

    Applied clang format

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -6,12 +6,15 @@ bool checkMax(T first, T second)
     return juce::jmax(first, second) == std::max(first, second);
 }
 
-int main() {
-    if (checkMax(1, 2)) {
+int main()
+{
+    if (checkMax(1, 2))
+    {
         std::cout << "SUCCESS" << std::endl;
         return 0;
     }
-    else {
+    else
+    {
         std::cout << "ERROR" << std::endl;
         return 1;
     }
