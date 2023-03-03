f4a51a21e7d9f9532d325a6ce067a83a25f66641
Damien Ronssin <damien.ronssin@gmail.com>
2023-03-04 00:37:12 +0100

    Added basic pitch json weights. Put in binary data and json loading from it. Added lookahead for each sequential model

--- Audio2Midi/Model/BasicPitchCNN.cpp
+++ Audio2Midi/Model/BasicPitchCNN.cpp
@@ -3,3 +3,58 @@
 //
 
 #include "BasicPitchCNN.h"
+
+using json = nlohmann::json;
+
+BasicPitchCNN::BasicPitchCNN()
+{
+    json json_cnn_contour = json::parse(BinaryData::cnn_contour_model_json,
+                                        BinaryData::cnn_contour_model_json
+                                            + BinaryData::cnn_contour_model_jsonSize);
+
+    mCNNContour.parseJson(json_cnn_contour);
+
+    json json_cnn_note = json::parse(BinaryData::cnn_note_model_json,
+                                     BinaryData::cnn_note_model_json
+                                         + BinaryData::cnn_note_model_jsonSize);
+
+    mCNNNote.parseJson(json_cnn_note);
+
+    json json_cnn_onset_input = json::parse(BinaryData::cnn_onset_1_model_json,
+                                            BinaryData::cnn_onset_1_model_json
+                                                + BinaryData::cnn_onset_1_model_jsonSize);
+
+    mCNNOnsetInput.parseJson(json_cnn_onset_input);
+
+    json json_cnn_onset_output = json::parse(
+        BinaryData::cnn_onset_2_model_json,
+        BinaryData::cnn_onset_2_model_json + BinaryData::cnn_onset_2_model_jsonSize);
+
+    mCNNOnsetOutput.parseJson(json_cnn_onset_output);
+}
+
+BasicPitchCNN::~BasicPitchCNN()
+{
+}
+
+void BasicPitchCNN::reset()
+{
+}
+
+int BasicPitchCNN::getNumFramesLookahead() const
+{
+    return 0;
+}
+
+void BasicPitchCNN::frameInference(std::vector<float>& inputData,
+                                   std::vector<float>& outContours,
+                                   std::vector<float>& outNotes,
+                                   std::vector<float>& outOnsets)
+{
+    ignoreUnused(inputData);
+    ignoreUnused(outContours);
+    ignoreUnused(outNotes);
+    ignoreUnused(outOnsets);
+    ignoreUnused(mInputArray);
+    ignoreUnused(mCurrentFrameIndex);
+}
--- Audio2Midi/Model/BasicPitchCNN.h
+++ Audio2Midi/Model/BasicPitchCNN.h
@@ -6,14 +6,34 @@
 #define BasicPitchCNN_h
 
 #include "RTNeural/RTNeural.h"
+#include "json.hpp"
+#include <JuceHeader.h>
 
 class BasicPitchCNN
 {
 public:
+    BasicPitchCNN();
+
+    ~BasicPitchCNN();
+
+    void reset();
+
+    int getNumFramesLookahead() const;
+
+    void frameInference(std::vector<float>& inputData,
+                        std::vector<float>& outContours,
+                        std::vector<float>& outNotes,
+                        std::vector<float>& outOnsets);
+
 private:
     static constexpr int NUM_HARMONICS = 8;
     static constexpr int NUM_FREQ_IN = 264;
     static constexpr int NUM_FREQ_OUT = 88;
+
+    size_t mCurrentFrameIndex = 0;
+
+    alignas(RTNEURAL_DEFAULT_ALIGNMENT)
+        std::array<float, NUM_FREQ_IN * NUM_HARMONICS> mInputArray;
 
     RTNeural::ModelT<
         float,
@@ -25,6 +45,8 @@ private:
         RTNeural::SigmoidActivationT<float, NUM_FREQ_IN>>
         mCNNContour;
 
+    static constexpr int mLookaheadCNNContour = 3;
+
     RTNeural::ModelT<float,
                      NUM_FREQ_IN,
                      NUM_FREQ_OUT,
@@ -34,6 +56,8 @@ private:
                      RTNeural::SigmoidActivationT<float, NUM_FREQ_OUT>>
         mCNNNote;
 
+    static constexpr int mLookaheadCNNNote = 6;
+
     RTNeural::ModelT<float,
                      NUM_FREQ_IN * NUM_HARMONICS,
                      32 * NUM_FREQ_OUT,
@@ -41,12 +65,17 @@ private:
                      RTNeural::ReLuActivationT<float, 32 * NUM_FREQ_OUT>>
         mCNNOnsetInput;
 
+    static constexpr int mLookaheadCNNOnsetInput = 2;
+
     RTNeural::ModelT<float,
                      33 * NUM_FREQ_OUT,
                      NUM_FREQ_OUT,
                      RTNeural::Conv2DT<float, 33, 1, NUM_FREQ_OUT, 3, 3, 1, 1, false>,
                      RTNeural::SigmoidActivationT<float, NUM_FREQ_OUT>>
         mCNNOnsetOutput;
+
+    static constexpr int mLookaheadCNNOnsetOutput = 1;
+
 };
 
 #endif // BasicPitchCNN_h
