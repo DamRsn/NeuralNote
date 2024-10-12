48090105298dd46830b61c1cdac35ecee413eff5
Damien Ronssin <damien.ronssin@gmail.com>
2024-10-12 08:16:34 +0200

    Build, sign and notarize installer

--- sign.macos.sh
+++ sign.macos.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-for dir in NeuralNote.{app,component,vst3}; do
+# First argument gives the path to the dir containing the .app, .component and .vst3
+PLUG_DIR=$1
+
+for dir in "$PLUG_DIR"/{Standalone/NeuralNote.app,AU/NeuralNote.component,VST3/NeuralNote.vst3}; do
 	if ! test -d "$dir"; then
 		>&2 echo "Could not find $dir"
 		exit 1
@@ -23,19 +26,20 @@ fi
 read -s -p "Enter your Apple ID password: " APPLE_PASSWORD
 echo
 
-chmod +x NeuralNote.{app,component,vst3}/Contents/MacOS/NeuralNote
-codesign --remove-signature NeuralNote.{app,component,vst3} || true
-codesign --entitlements entitlements.plist --options=runtime -s "$signingID" NeuralNote.{app,component,vst3}
+chmod +x "$PLUG_DIR"/{Standalone/NeuralNote.app,AU/NeuralNote.component,VST3/NeuralNote.vst3}/Contents/MacOS/NeuralNote
+codesign --remove-signature "$PLUG_DIR"/{Standalone/NeuralNote.app,AU/NeuralNote.component,VST3/NeuralNote.vst3} || true
+codesign --entitlements entitlements.plist --options=runtime -s "$signingID" "$PLUG_DIR"/{Standalone/NeuralNote.app,AU/NeuralNote.component,VST3/NeuralNote.vst3}
 
-for dir in NeuralNote.{app,component,vst3}; do
-	plist="$dir/Contents/Info.plist"
-	bundleID=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$plist")
-	if test -z "$bundleID"; then
-		>&2 echo "Could not find CFBundleIdentifier in $plist"
-		exit 1
-	fi
-	ditto -c -k --sequesterRsrc --keepParent "$dir" "$dir.zip"
-	xcrun notarytool submit --apple-id "$APPLE_USERNAME" --team-id "$APPLE_TEAMID" --password "$APPLE_PASSWORD" --wait "$dir.zip"
-	xcrun stapler staple "$dir"
-	rm "$dir.zip"
-done
+# Build installer
+packagesbuild --project Installers/Mac/NeuralNote.pkgproj
+mv Installers/Mac/build/NeuralNote.pkg Installers/Mac/build/NeuralNote_unsigned.pkg
+
+# Sign installer
+product_sign_ID=$(echo "$signingID" | sed 's/Application/Installer/')
+#productsignID=$(security find-identity -v -p basic | grep "Developer ID Installer" | head -1 | cut -d'"' -f2)
+productsign --sign "$product_sign_ID" Installers/Mac/build/NeuralNote_unsigned.pkg Installers/Mac/build/NeuralNote.pkg
+rm Installers/Mac/build/NeuralNote_unsigned.pkg
+
+# Notarize the pkg and staple it
+xcrun notarytool submit --apple-id "$APPLE_USERNAME" --team-id "$APPLE_TEAMID" --password "$APPLE_PASSWORD" --wait Installers/Mac/build/NeuralNote.pkg
+xcrun stapler staple Installers/Mac/NeuralNote.pkg
